aff29b7e82c6c3ab3547de7e8791c1c71a707195
Claes 'Letharion' Gyllensvärd <letharion@gmail.com>
2021-05-03 07:07:08 +0200

    Drop lots of old un-unsed ebuilds

--- scripts/make_provision_release.sh
+++
@@ -1,6 +0,0 @@
-git clone --branch 7.x-3.x http://git.drupal.org/project/provision.git
-FILENAME="provision-7.x-2.$(date +%Y%m%d).tar.gz"
-tar caf ${FILENAME} provision --exclude .git*
-mv ${FILENAME} /usr/portage/distfiles
-
-echo "Created distfile for ${FILENAME}"
